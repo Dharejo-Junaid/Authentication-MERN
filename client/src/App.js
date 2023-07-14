a91904759bf2727d6bdb1ce2683f19b590a7c0fd
Dharejo-Junaid <junaidali.100190@gmail.com>
2023-07-14 08:18:02 -0700

    Remove logs

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
 import SLBar from "./components/slBar/SLBar";
 import LoginForm from "./components/loginForm/LoginForm";
 import SignupForm from "./components/signupForm/SignupForm";
 import Verity from "./components/verify/Verify";
 import axios from "axios";
+import { useState } from "react";
 import { Routes, Route, useSearchParams } from "react-router-dom";
 
 const App = () => {
@@ -34,11 +34,11 @@ const App = () => {
   }
 
   const token = localStorage.getItem("token");
-    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoYXNoSUQiOiIkMmIkMTAkSEh0YWQuZEJiUUttakpndUJmdGI5Lml5M2FSWERRWVlZOFpFWk5FRlFBcEVSaFZrNU16QmUiLCJpYXQiOjE2ODg4NDk0MjZ9.ALxFeJKajIDt3mQI89y4nFIQJhnBalNBvySizJ41HnI";
+
   if(token) {
     axios.post("/login", {token: token}).then((res) => {
-      const { success, message} = res.data;
-      console.log(success, message);
+      // const { success, message} = res.data;
+      // redirect to dashboard;
     });
   }
 
